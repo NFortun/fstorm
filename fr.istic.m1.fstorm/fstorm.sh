5b5efa5bd97a3fde29be118073d9e074508876ab
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2017-04-25 19:39:37 +0200

    Add the fstorm.sh executable script

---
+++ fr.istic.m1.fstorm/fstorm.sh
@@ -0,0 +1,5 @@
+ARGS=$*
+ECLIPSE_DIR="$HOME/eclipse"
+WORKSPACE="$HOME/workspace"
+
+echo "`/usr/lib/jvm/java-8-jdk/bin/java -Declipse.pde.launch=true -Dfile.encoding=UTF-8 -classpath ${ECLIPSE_DIR}/plugins/org.eclipse.equinox.launcher_1.3.201.v20161025-1711.jar org.eclipse.equinox.launcher.Main -launcher ${ECLIPSE_DIR}/eclipse -name Eclipse -showsplash 600 -application fr.istic.m1.fstorm.fstorm -data ./runtime-Fstormapplication -configuration "file:${WORKSPACE}/.metadata/.plugins/org.eclipse.pde.core/Fstorm application/" -dev "file:${WORKSPACE}/.metadata/.plugins/org.eclipse.pde.core/Fstorm application/dev.properties" -os linux -ws gtk -arch x86_64 -nl fr_FR ${ARGS}`"
